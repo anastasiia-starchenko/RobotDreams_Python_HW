cc994acb7699837deaa4b7e6dc54228121243e9b
anastasiia-starchenko <starchenko.anastasiia.r@gmail.com>
2025-04-08 22:56:52 +0300

    dz29

---
+++ database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+SQLALCHEMY_DATABASE_URL = "postgresql://anastasiiastarchenko:Qwerty@localhost:5432/lms_db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
